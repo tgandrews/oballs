7bfb3d60580f8d52f9c2babf7d82d70004b3d964
Tom Andrews <git@tomandrews.co.uk>
2014-04-17 10:01:51 +0100

    Generate stats off graphs

--- lib/stats_generator.rb
+++ lib/stats_generator.rb
@@ -7,9 +7,31 @@ class StatsGenerator
     @app_graph = graph
   end
 
-  def generate(session_count: 1)
-    #current_location = 'home'
-    [[]]
+  def generate(session_count: 1, session_length: 5)
+    sessions = []
+
+    for i in 1..session_count
+      session = ['home']
+      current_location = STARTING_POINT
+
+      for i in 1..(session_length - 1)
+        possible_destinations = @app_graph.adjacent(current_location)
+        possible_destinations.push 'exit'
+
+        next_index = @random.rand possible_destinations.length
+
+        next_location = possible_destinations[next_index]
+        if next_location == 'exit'
+          break
+        end
+        session.push next_location
+        current_location = next_location
+      end
+
+      sessions.push session
+    end
+
+    sessions
   end
 
   private
--- test/stats_generator_test.rb
+++ test/stats_generator_test.rb
@@ -6,7 +6,7 @@ require 'minitest/autorun'
 class StatsGeneratorTest < Minitest::Unit::TestCase
   def setup
     graph =  ApplicationGraphCreator.create
-    @stats_generator = StatsGenerator.new(graph: graph, seed: 12345)
+    @stats_generator = StatsGenerator.new(graph: graph, seed: 12345678911)
   end
 
   def test_creates_correct_number_of_sessions
@@ -14,9 +14,19 @@ class StatsGeneratorTest < Minitest::Unit::TestCase
     assert_equal sessions.length, 1
   end
 
-  # def test_creates_example_session
-  #   sessions = @stats_generator.generate
-  #   first_session = sessions.first
-  #   assert_equal ['home'], first_session
-  # end
+  def test_creates_example_session
+    sessions = @stats_generator.generate
+    first_session = sessions.first
+    assert_equal ["home", "categories", "playback", "categories", "home"], first_session
+  end
+
+  def test_creates_three_example_sessions
+    sessions = @stats_generator.generate session_count: 3
+
+    second_session = sessions[1]
+    assert_equal ["home", "categories", "home"], second_session
+
+    third_session = sessions[2]
+    assert_equal ["home"], third_session
+  end
 end
